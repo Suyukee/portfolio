763070f53685d01249996bbf9c383d927aef4529
Suyukee <diefuck@ya.ru>
2024-09-03 15:08:09 +0300

    Тест viewport для телефона

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,17 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import { ThemeProvider } from '@mui/material';
-import { Comic_Neue } from 'next/font/google';
 import './globals.css';
 import theme from '@/components/MaterialThemeProvider';
 
 export const metadata: Metadata = {
 	title: 'Portfolio website',
 	description: 'Portfolio website Козырева Владимира',
+};
+
+export const viewport: Viewport = {
+	width: 'device-width',
+	initialScale: 1,
 };
 
 export default function RootLayout({
